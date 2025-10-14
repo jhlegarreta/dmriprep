a8bd204a09bd860a0e739b3cfdaa9d3b924578c0
Oscar Esteban <code@oscaresteban.es>
2025-10-14 16:41:29 +0200

    fix: unused import in ``docs/conf.py``

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import re
-
 from packaging.version import Version
 
 import dmriprep as _dmriprep
